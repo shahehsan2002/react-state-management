b3c184e704654728fe70143d92e07b3a271ca81a
Shah Ehsan <shahehsan2002@gmail.com>
2024-08-18 01:30:33 +0600

    Counnter With Class Components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import CounterWithClass from "./components/CounterWithClassComponent"
 
 function App() {
   
 
   return (
     <>
-      <h1 className="text-4xl text-center underline" >React state management</h1>
+      <CounterWithClass/>
     </>
   )
 }
